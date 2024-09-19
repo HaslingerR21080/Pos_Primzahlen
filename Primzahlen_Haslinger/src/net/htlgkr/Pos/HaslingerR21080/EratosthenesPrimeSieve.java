dc81ba834633dc4d779bc483282c0be5f7f037b8
Robin Haslinger <HaslingerR21080@sus.htl-grieskirchen.at>
2024-09-19 14:56:55 +0200

    Methods created

--- Primzahlen_Haslinger/src/net/htlgkr/Pos/HaslingerR21080/EratosthenesPrimeSieve.java
+++ Primzahlen_Haslinger/src/net/htlgkr/Pos/HaslingerR21080/EratosthenesPrimeSieve.java
@@ -1,4 +1,23 @@
 package net.htlgkr.Pos.HaslingerR21080;
 
-public class EratosthenesPrimeSieve {
+public class EratosthenesPrimeSieve implements PrimeSieve{
+    private int max;
+    private  boolean[] PrimesAndNoPrimes;
+
+    @Override
+    public boolean isPrime(int a) {
+        if (a < 2){
+            return false;
+        }
+        for (int i = 2;i<= Math.sqrt(max); i++){
+            
+        }
+
+        return false;
+    }
+
+    @Override
+    public void printPrimes() {
+
+    }
 }
--- Primzahlen_Haslinger/src/net/htlgkr/Pos/HaslingerR21080/PrimeSieve.java
+++ Primzahlen_Haslinger/src/net/htlgkr/Pos/HaslingerR21080/PrimeSieve.java
@@ -1,4 +1,6 @@
 package net.htlgkr.Pos.HaslingerR21080;
 
 public interface PrimeSieve {
+    boolean isPrime (int a);
+    void printPrimes();
 }
